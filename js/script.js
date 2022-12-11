7bdf9143fde513e111b215eab4a6699221624e1a
Sergey Trifonov <se.trif0nov@yandex.com>
2022-12-11 18:28:03 +0300

    create modal

--- js/script.js
+++ js/script.js
@@ -101,4 +101,38 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('.timer', deadline);
+
+    // create modalWindow
+
+    const modalTrig = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          closeBtn = document.querySelector('[data-close]');
+
+    modalTrig.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    closeBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    this.document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
