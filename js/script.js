82f86814e63b18c8a570b4d22a8aba8045726761
Sergey Trifonov <se.trif0nov@yandex.com>
2022-12-13 17:23:08 +0300

    use request xmlHttp with formData()

--- js/script.js
+++ js/script.js
@@ -216,4 +216,50 @@ window.addEventListener('DOMContentLoaded', function() {
         '.menu .container',
         'menu__item',
     ).render();
+
+    // send data on server "Forms"
+
+    const forms = document.querySelectorAll("form");
+
+    const message = {
+        loading: 'Загрузочка',
+        success: 'Скоро мы с тобой свяжемся',
+        failure: 'Что-то сломалось :)'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+
+            const formData = new FormData(form);
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+    }
 });
